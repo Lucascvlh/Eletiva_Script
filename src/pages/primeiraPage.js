801c09971e5a7e2176d8f26df192f3db8bb283fe
Lucascvlh <luikinha_henrique@hotmail.com>
2023-08-23 20:22:20 -0300

    Terceira Aula

---
+++ src/pages/primeiraPage.js
@@ -0,0 +1,8 @@
+export default function primeiraPage(){
+    return (
+        <div>
+            <h1>Hello World!!</h1>
+            <h2>Aprendendo Next.JS</h2>
+        </div>
+    )
+}
---
+++ src/pages/segundaPage.jsx
@@ -0,0 +1,8 @@
+export default function segundaPage(){
+    return(
+        <div className="container">
+            <h1>Trabalhando com números</h1>
+            5 * 5 = {5*5}
+        </div>
+    )
+}
